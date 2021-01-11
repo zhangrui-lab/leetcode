2f572574c58a5d042b59c07f7cc825a6723ee2e1
luoluoluoya <18684085840@163.com>
2021-01-11 20:45:56 +0800

    正则表达式匹配

--- regularExpressionMatching/regularExpressionMatching.h
+++ regularExpressionMatching/regularExpressionMatching.h
@@ -24,13 +24,21 @@
 #include <vector>
 #include <string>
 
-bool isMatch(std::string s, std::string p) {
+bool isMatch(string s, string p) {
     int m = s.size();
     int n = p.size();
+
     auto matches = [&](int i, int j) {
-        return i == 0 || p[j - 1] == '.' || s[i - 1] == p[j - 1];
+        if (i == 0) {
+            return false;
+        }
+        if (p[j - 1] == '.') {
+            return true;
+        }
+        return s[i - 1] == p[j - 1];
     };
-    std::vector<std::vector<int>> f(m + 1, std::vector<int>(n + 1));
+
+    vector<vector<int>> f(m + 1, vector<int>(n + 1));
     f[0][0] = true;
     for (int i = 0; i <= m; ++i) {
         for (int j = 1; j <= n; ++j) {
@@ -39,7 +47,8 @@ bool isMatch(std::string s, std::string p) {
                 if (matches(i, j - 1)) {
                     f[i][j] |= f[i - 1][j];
                 }
-            } else {
+            }
+            else {
                 if (matches(i, j)) {
                     f[i][j] |= f[i - 1][j - 1];
                 }
