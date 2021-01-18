51b18721d575bcecd81007ab2e59f3b6a2e82ff0
luoluoluoya <18684085840@163.com>
2021-01-18 16:18:19 +0800

    最大子序和

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 
 #include "solveNQueens/solveNQueens.h"
 #include "totalNQueens/totalNQueens.h"
+#include "maximumSubarray/maximumSubarray.h"
 
 using namespace std;
 
 int main() {
-    cout << totalNQueens(4);
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << maxSubArray(nums);
 //    auto resu = solveNQueens2(4);
 //    for (int i = 0; i < resu.size(); ++i) {
 //        for (int j = 0; j < resu[i].size(); ++j) {
