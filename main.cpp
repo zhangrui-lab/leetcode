179ccc9124715b92c9994e9fd290a9f260a1d4aa
luoluoluoya <18684085840@163.com>
2021-01-18 15:37:41 +0800

    N皇后 II

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 
 #include "solveNQueens/solveNQueens.h"
+#include "totalNQueens/totalNQueens.h"
 
 using namespace std;
 
 int main() {
-    auto resu = solveNQueens2(4);
-    for (int i = 0; i < resu.size(); ++i) {
-        for (int j = 0; j < resu[i].size(); ++j) {
-            printf("%s ", resu[i][j].c_str());
-        }
-        printf("\n");
-    }
+    cout << totalNQueens(4);
+//    auto resu = solveNQueens2(4);
+//    for (int i = 0; i < resu.size(); ++i) {
+//        for (int j = 0; j < resu[i].size(); ++j) {
+//            printf("%s ", resu[i][j].c_str());
+//        }
+//        printf("\n");
+//    }
 }
