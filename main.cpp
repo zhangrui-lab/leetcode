e85a0667897b6162ef56931d160389ab9efe2ec8
luoluoluoya <18684085840@163.com>
2021-01-19 15:05:26 +0800

    旋转链表

--- main.cpp
+++ main.cpp
@@ -8,12 +8,19 @@
 #include "lengthOfLastWord/lengthOfLastWord.h"
 #include "generateMatrix/generateMatrix.h"
 #include "permutationSequence/permutationSequence.h"
+#include "rotateRight/rotateRight.h"
 
 using namespace std;
 
 int main() {
-    // 123495786
-    cout << getPermutation3(9, 100);
+//    ListNode* node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    ListNode* node = new ListNode(0, new ListNode(1, new ListNode(2)));
+    node = rotateRight(nullptr, 0);
+    while (node) {
+        cout << node->val << "->";
+        node = node->next;
+    }
+
 //    auto resu =  generateMatrix(3);
 //    for (int i = 0; i < resu.size(); ++i) {
 //        for (int j = 0; j < resu[i].size(); ++j) {
