b7f19b4100480b095394f3c4c18240354b6a18ba
luoluoluoya <18684085840@163.com>
2021-01-22 13:50:21 +0800

    删除排序链表中的重复元素

---
+++ removeDuplicatesFromSortedList/removeDuplicatesFromSortedList.h
@@ -0,0 +1,29 @@
+//
+// Created by 张锐 on 2021/1/22.
+//
+
+#ifndef ALGORITHM_REMOVEDUPLICATESFROMSORTEDLIST_H
+#define ALGORITHM_REMOVEDUPLICATESFROMSORTEDLIST_H
+
+/*
+ * 83. 删除排序链表中的重复元素
+ *  给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
+
+#include "../common.h"
+
+ListNode *removeDuplicatesFromSortedList(ListNode *head) {
+    if (!head)
+        return head;
+    ListNode *header = new ListNode(0, head), *prev = header;
+    for (ListNode *node = head->next; node; node = node->next) {
+        if (prev->next->val != node->val) {
+            prev->next->next = node;
+            prev = prev->next;
+        }
+    }
+    prev->next->next = nullptr;
+    return header->next;
+}
+
+#endif //ALGORITHM_REMOVEDUPLICATESFROMSORTEDLIST_H
--- removeDuplicatesFromSortedList2/removeDuplicatesFromSortedList2.h
+++ removeDuplicatesFromSortedList2/removeDuplicatesFromSortedList2.h
@@ -8,28 +8,9 @@
 /*
  * 82. 删除排序链表中的重复元素 II
  *  给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中没有重复出现的数字。
- *
- * 1. 去重，使当前序列不存在重复字符
- * 2. 删除所有含有重复数字的节点，只保留原始链表中没有重复出现的数字
  */
 
 #include "../common.h"
-
-
-ListNode *removeDuplicatesFromSortedList21(ListNode *head) {
-    if (!head)
-        return head;
-    ListNode *header = new ListNode(0, head);
-    ListNode *prev = header;
-    for (ListNode *node = head->next; node; node = node->next) {
-        if (prev->next->val != node->val) {
-            prev->next->next = node;
-            prev = prev->next;
-        }
-    }
-    prev->next->next = nullptr;
-    return header->next;
-}
 
 ListNode *removeDuplicatesFromSortedList22(ListNode *head) {
     ListNode *header = new ListNode, *prev = header;
