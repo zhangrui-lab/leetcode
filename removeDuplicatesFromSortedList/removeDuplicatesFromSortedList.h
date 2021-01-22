f16470f77f78068f1f28c46b1e73fa737953c9ca
luoluoluoya <18684085840@163.com>
2021-01-22 13:56:04 +0800

    删除排序链表中的重复元素

--- removeDuplicatesFromSortedList/removeDuplicatesFromSortedList.h
+++ removeDuplicatesFromSortedList/removeDuplicatesFromSortedList.h
@@ -13,6 +13,18 @@
 #include "../common.h"
 
 ListNode *removeDuplicatesFromSortedList(ListNode *head) {
+    ListNode* curr = head;
+    while (curr && curr->next) {
+        if (curr->val == curr->next->val) {
+            curr->next = curr->next->next;
+        } else {
+            curr = curr->next;
+        }
+    }
+    return head;
+}
+
+ListNode *removeDuplicatesFromSortedList(ListNode *head) {
     if (!head)
         return head;
     ListNode *header = new ListNode(0, head), *prev = header;
