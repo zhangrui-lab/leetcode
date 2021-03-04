eca2d5950c8315b5bb3f98c18370bf03917a9265
luoluoluoya <18684085840@163.com>
2021-03-04 17:20:58 +0800

    179. 最大数

---
+++ largestNumber/largestNumber.h
@@ -0,0 +1,24 @@
+//
+// Created by 张锐 on 2021/3/4.
+//
+
+#ifndef ALGORITHM_LARGESTNUMBER_H
+#define ALGORITHM_LARGESTNUMBER_H
+
+/*
+ * 179. 最大数
+ *  给定一组非负整数 nums，重新排列它们每个数字的顺序（每个数字不可拆分）使之组成一个最大的整数。
+ *  注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+ *
+ *  todo
+ */
+
+#include <string>
+
+
+
+std::string largestNumber(std::vector<int>& nums) {
+}
+
+
+#endif //ALGORITHM_LARGESTNUMBER_H
