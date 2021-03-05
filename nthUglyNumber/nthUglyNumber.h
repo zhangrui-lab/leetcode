5444e5c964f7db040ae9b0be93a4db24d619ad06
luoluoluoya <18684085840@163.com>
2021-03-05 21:50:36 +0800

    264. 丑数 II

---
+++ nthUglyNumber/nthUglyNumber.h
@@ -0,0 +1,18 @@
+//
+// Created by 张锐 on 2021/3/5.
+//
+
+#ifndef ALGORITHM_NTHUGLYNUMBER_H
+#define ALGORITHM_NTHUGLYNUMBER_H
+
+/*
+ * 264. 丑数 II
+ *  编写一个程序，找出第 n 个丑数。
+ *  丑数就是质因数只包含 2, 3, 5 的正整数。
+ *  todo
+ */
+
+int nthUglyNumber(int n) {
+}
+
+#endif //ALGORITHM_NTHUGLYNUMBER_H
