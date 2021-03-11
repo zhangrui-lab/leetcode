2d841107d37fc324f415949cc4763f41db5f4d11
luoluoluoya <18684085840@163.com>
2021-03-11 14:10:03 +0800

    1690. 石子游戏 VII

--- stoneGame/stoneGame.h
+++ stoneGame/stoneGame.h
@@ -33,11 +33,9 @@ bool stoneGame(std::vector<int> &piles) {
     std::vector<std::vector<int>> dp(n, std::vector<int>(n));
     for (int i = 0; i < n; ++i)
         dp[i][i] = piles[i];
-    for (int i = n - 2; i >= 0; i--) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = n - 2; i >= 0; i--)
+        for (int j = i + 1; j < n; j++)
             dp[i][j] = std::max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
-        }
-    }
     return dp[0][n - 1] >= 0;
 }
 
