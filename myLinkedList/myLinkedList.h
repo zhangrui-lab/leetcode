c94bcd4ca55c4661790103f218ab57eb60573c58
luoluoluoya <18684085840@163.com>
2021-03-11 16:17:25 +0800

    707. 设计链表

---
+++ myLinkedList/myLinkedList.h
@@ -0,0 +1,113 @@
+//
+// Created by 张锐 on 2021/3/11.
+//
+
+#ifndef ALGORITHM_MYLINKEDLIST_H
+#define ALGORITHM_MYLINKEDLIST_H
+
+/*
+ * 707. 设计链表
+ *  设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+ *  在链表类中实现这些功能：
+ *  get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+ *  addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+ *  addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+ *  addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+ *  deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+ */
+
+#include <cassert>
+
+// 未实现析构功能
+class MyLinkedList {
+public:
+    /** Initialize your data structure here. */
+    MyLinkedList() : header(new ListNode), tail(new ListNode(0, header, nullptr)), size(0) {
+        header->next = tail;
+    }
+
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    int get(int index) {
+        if (index >= size)
+            return -1;
+        return getNode(index)->val;
+    }
+
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    void addAtHead(int val) {
+        insertAsAfter(val, header);
+    }
+
+    /** Append a node of value val to the last element of the linked list. */
+    void addAtTail(int val) {
+        insertAsBefore(val, tail);
+    }
+
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    void addAtIndex(int index, int val) {
+        if (index > size)
+            return;
+        else if (index == size) {
+            addAtTail(val);
+        } else if (index == 0) {
+            addAtHead(val);
+        } else {
+            insertAsBefore(val, getNode(index));
+        }
+    }
+
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    void deleteAtIndex(int index) {
+        if (index >= size)
+            return;
+        deleteNode(getNode(index));
+    }
+
+private:
+    class ListNode {
+    public:
+        int val;
+        ListNode *prev;
+        ListNode *next;
+
+        ListNode(int val = 0, ListNode *prev = nullptr, ListNode *next = nullptr) : val(val), prev(prev), next(next) {}
+    };
+
+    ListNode *header;
+    ListNode *tail;
+    int size;
+
+    ListNode *getNode(int index) {
+        assert(index < size);
+        ListNode *node = header->next;
+        while (index--)
+            node = node->next;
+        return node;
+    }
+
+    ListNode *insertAsBefore(int val, ListNode *node) {
+        size++;
+        ListNode *newNode = new ListNode(val, node->prev, node);
+        node->prev->next = newNode;
+        node->prev = newNode;
+        return newNode;
+    }
+
+    ListNode *insertAsAfter(int val, ListNode *node) {
+        size++;
+        ListNode *newNode = new ListNode(val, node, node->next);
+        node->next->prev = newNode;
+        node->next = newNode;
+        return newNode;
+    }
+
+    int deleteNode(ListNode *node) {
+        size--;
+        int val = node->val;
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        return val;
+    }
+};
+
+#endif //ALGORITHM_MYLINKEDLIST_H
