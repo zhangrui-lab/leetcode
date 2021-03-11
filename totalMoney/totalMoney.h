5737f89610616e190d316ad8c567954a98ee259e
luoluoluoya <18684085840@163.com>
2021-03-11 16:57:37 +0800

    1716. 计算力扣银行的钱

--- totalMoney/totalMoney.h
+++ totalMoney/totalMoney.h
@@ -35,4 +35,14 @@ int totalMoney(int n) {
     return res;
 }
 
+int totalMoney1(int n) {
+    int total = 0, week = 0, offset = 0;
+    for (int i = 0; i < n; i++) {
+        week = i / 7;
+        offset = i % 7 + 1;
+        total += week + offset;
+    }
+    return total;
+}
+
 #endif //ALGORITHM_TOTALMONEY_H
