340978eba3f89e5a8c6cd4d4fba7344fe939e93a
luoluoluoya <18684085840@163.com>
2021-07-21 04:29:33 -0500

    146. LRU 缓存机制

---
+++ LRUCache/LRUCache.h
@@ -0,0 +1,113 @@
+//
+// Created by Administrator on 2021/7/21.
+//
+
+#ifndef ALGORITHM_LRUCACHE_H
+#define ALGORITHM_LRUCACHE_H
+
+/*
+ * 146. LRU 缓存机制
+ *  运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
+ *  实现 LRUCache 类：
+ *      LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+ *      int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ *      void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ * 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
+ *
+ * 解决思路:
+ *  使用一个HashMap保存键值对,另一个HashMap保存存在的key当前访问的时间戳.
+ */
+
+#include <unordered_map>
+
+using namespace std;
+
+struct DLinkedNode {
+    int key, value;
+    DLinkedNode *prev;
+    DLinkedNode *next;
+
+    DLinkedNode() : key(0), value(0), prev(nullptr), next(nullptr) {}
+
+    DLinkedNode(int _key, int _value) : key(_key), value(_value), prev(nullptr), next(nullptr) {}
+};
+
+class LRUCache {
+private:
+    unordered_map<int, DLinkedNode *> cache;
+    DLinkedNode *head;
+    DLinkedNode *tail;
+    int size;
+    int capacity;
+
+public:
+    LRUCache(int _capacity) : capacity(_capacity), size(0) {
+        // 使用伪头部和伪尾部节点
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (!cache.count(key)) {
+            return -1;
+        }
+        // 如果 key 存在，先通过哈希表定位，再移到头部
+        DLinkedNode *node = cache[key];
+        moveToHead(node);
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (!cache.count(key)) {
+            // 如果 key 不存在，创建一个新的节点
+            DLinkedNode *node = new DLinkedNode(key, value);
+            // 添加进哈希表
+            cache[key] = node;
+            // 添加至双向链表的头部
+            addToHead(node);
+            ++size;
+            if (size > capacity) {
+                // 如果超出容量，删除双向链表的尾部节点
+                DLinkedNode *removed = removeTail();
+                // 删除哈希表中对应的项
+                cache.erase(removed->key);
+                // 防止内存泄漏
+                delete removed;
+                --size;
+            }
+        } else {
+            // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
+            DLinkedNode *node = cache[key];
+            node->value = value;
+            moveToHead(node);
+        }
+    }
+
+    void addToHead(DLinkedNode *node) {
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void removeNode(DLinkedNode *node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToHead(DLinkedNode *node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    DLinkedNode *removeTail() {
+        DLinkedNode *node = tail->prev;
+        removeNode(node);
+        return node;
+    }
+};
+
+
+#endif //ALGORITHM_LRUCACHE_H
