c57ca5af23b4d984d9815f0a4c1f9c842bd15b21
luoluoluoya <18684085840@163.com>
2021-07-28 03:13:11 -0500

    173. 二叉搜索树迭代器

--- BSTIterator/BSTIterator.h
+++ BSTIterator/BSTIterator.h
@@ -12,6 +12,10 @@
  *  boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
  *  int next()将指针向右移动，然后返回指针处的数字。
  *  注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ *
+ * 求解策略:
+ *  1. stack
+ *  2. 获取并保存完整中序遍历序列
  */
 
 #include <stack>
@@ -47,5 +51,37 @@ private:
         }
     }
 };
+
+class BSTIterator2 {
+private:
+    void inorder(TreeNode *root, vector<int> &res) {
+        if (!root) {
+            return;
+        }
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+
+    vector<int> arr;
+    int idx;
+public:
+    BSTIterator2(TreeNode *root) : idx(0), arr(inorderTraversal(root)) {}
+
+    int next() {
+        return arr[idx++];
+    }
+
+    bool hasNext() {
+        return (idx < arr.size());
+    }
+};
+
 
 #endif //ALGORITHM_BSTITERATOR_H
