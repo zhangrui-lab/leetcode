0dfd41247bcddcf074f44b9cb3372a460da07b78
zhangrui <zhangrui@anmaicloud.com>
2021-08-26 13:44:52 +0800

    222. 完全二叉树的节点个数: 二分查找策略

--- countNodes/countNodes.h
+++ countNodes/countNodes.h
@@ -13,8 +13,16 @@
  * 求解策略:
  *  1. 图遍历: dfs, bfs, stack替换迭代等...
  *  2. 二分查找策略+位结构:
- *      a. 完整树的middle节点: root右子节点的最左侧节点.
- *      b. 查看树的middle节点是否高度为树高, 若为树高, 则在其右子树进行迭代, 若不为树高, 则在其左子树中进行迭代.
+ *                                  1(1)
+ *                     10(2)                              11(3)
+ *             100(4)          101(5)            110(6)               111(7)
+ *       1000(8)  1001(9) 1010(10) 1011(11) 1100(12)  1101(13)  1110(14)   1111(15)
+ * 10000(16)  10001(17) ......
+ *      树中第 h 层的节点数为: 0 ~ 2{h}       (h>0)
+ *                          0              (h=0)
+ *      树中第 h 层的节点号为: 2^{h} ~ 2{h+1}-1 (h>0)
+ *                          1              (h=0)
+ *      节点n的编号位图可以决定其定位方式: 去除最高位1后. 从根节点开始, 依次获取最高位值x, x为0时向左,x为1时向右.
  */
 
 #include "../common.h"
@@ -91,5 +99,32 @@ int countNodes3(TreeNode *root) {
     }
     return low;
 }
+
+int countNodes4(TreeNode *root) {
+    if (root == nullptr) return 0;
+    int height = 0;
+    TreeNode *node = root;
+    while (node->left) {
+        height++;
+        node = node->left;
+    }
+    auto exists = [](TreeNode *root, int height, int node) {
+        for (int bits = 1 << (height - 1); bits != 0; bits >>= 1) {
+            root = (bits & node) ? root->right : root->left;
+        }
+        return root != nullptr;
+    };
+    int low = 1 << height, high = (1 << (height + 1)) - 1, mid;
+    while (low < high) {
+        mid = (high - low + 1) / 2 + low;
+        if (exists(root, height, mid)) {
+            low = mid;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
 
 #endif //ALGORITHM_COUNTNODES_H
